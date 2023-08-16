35c2d1de300c555d8efe6f0300db45f2c93d14b1
sanches13 <plotnikov_alexey@list.ru>
2023-08-16 13:30:29 +0300

    some_changes

--- currency_rates.py
+++ currency_rates.py
@@ -1,24 +1,27 @@
-""" 
-Считываем два аргумента из консоли
-Проверяем их на корректность
-Определяем значения параметров --code и --date
-Подставляем date в https://www.cbr.ru/scripts/XML_daily.asp?date_req=dd/mm/yyyy
-Ищем значение курса для code
-"""
-import sys, requests, re
+import sys, requests, re, argparse
+from datetime import datetime
 
-def check_arguments_format(options) -> bool:
-    options_format = re.compile(r"--(code|date)=*")
-    for option in options:
-        if options_format.search(option).group() == []:
-            return False
-    return True
+def arguments_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--code', type=str, required=True)
+    parser.add_argument('--date', type=str, required=True)
+    return parser.parse_args()
 
-def arguments_correct(options) -> bool:
-    if check_arguments_format(options):
-        pass
+def is_code_correct(code) -> bool:
+    iso_list = requests.get("https://www.cbr.ru/scripts/XML_valFull.asp").text
+    iso_char_code = re.compile(r'<ISO_Char_Code>([A-Z]{3})</ISO_Char_Code>')
+    return code in iso_char_code.findall(iso_list)
 
-if len(sys.argv) != 3 or arguments_correct(sys.argv[1:]):
-    print(f"Usage {sys.argv[0]} --code=ISO_FORMAT --date=DD-MM-YYYY")
-    sys.exit()
-    
+def is_date_correct(date) -> bool:
+    try:
+        datetime.strptime(date, '%d-%m-%Y')
+        return True
+    except ValueError:
+        return False
+
+def main():
+    args = arguments_parser()
+    print(is_date_correct(args.date))
+
+if __name__ == "__main__":
+    main()
